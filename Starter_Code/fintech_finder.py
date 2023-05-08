4cbe7388b28db3d46d40ce1fdd65170c70f308ac
Romain Williams <118142731+rbawilliams@users.noreply.github.com>
2023-05-08 11:22:47 -0400

    Updated

---
+++ Starter_Code/fintech_finder.py
@@ -0,0 +1,63 @@
+# Imports
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+from bip44 import Wallet
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "e00e3fa4-68c1-4e94-adc0-ebde0e4e1860",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Imports\n",
+    "import os\n",
+    "import requests\n",
+    "from dotenv import load_dotenv\n",
+    "\n",
+    "load_dotenv()\n",
+    "from bip44 import Wallet\n",
+    "from web3 import Account\n",
+    "from web3 import middleware\n",
+    "from web3.gas_strategies.time_based import medium_gas_price_strategy"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "id": "3d18073c-5550-437a-841c-c7a4d3c0c07e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
